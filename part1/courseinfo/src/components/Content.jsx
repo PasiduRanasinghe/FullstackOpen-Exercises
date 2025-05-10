8e88a16b83d857f377c6531fca5cf26199ab5a64
Pasidu Ranasinghe <104724063+PasiduRanasinghe@users.noreply.github.com>
2025-05-10 23:50:14 +0530

    Exercise 1.1 bug fixed

--- part1/courseinfo/src/components/Content.jsx
+++ part1/courseinfo/src/components/Content.jsx
@@ -5,10 +5,10 @@ function Content(props) {
         {props.parts.part1} {props.exercises.exercises1}
       </p>
       <p>
-        {props.parts.part2} {props.exercises.exercises1}
+        {props.parts.part2} {props.exercises.exercises2}
       </p>
       <p>
-        {props.parts.part3} {props.exercises.exercises1}
+        {props.parts.part3} {props.exercises.exercises3}
       </p>
     </>
   );
