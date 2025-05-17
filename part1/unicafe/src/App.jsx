f299ca8a9b0540420a57522fe27499407374f760
Pasidu Ranasinghe <104724063+PasiduRanasinghe@users.noreply.github.com>
2025-05-17 23:23:49 +0530

    Exercise 1.6 final

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,7 +5,28 @@ function App() {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <>code here</>;
+  const handleClickGood = () => {
+    setGood(good + 1);
+  };
+  const handleClickNeutral = () => {
+    setNeutral(neutral + 1);
+  };
+  const handleClickBad = () => {
+    setBad(bad + 1);
+  };
+
+  return (
+    <>
+      <h1>give feedback</h1>
+      <button onClick={handleClickGood}>good</button>
+      <button onClick={handleClickNeutral}>neutral</button>
+      <button onClick={handleClickBad}>bad</button>
+      <h1>statistic</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </>
+  );
 }
 
 export default App;
