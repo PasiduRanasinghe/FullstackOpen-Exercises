2d200f6569ea8b3d765d4fbf178b082faff9c815
Pasidu Ranasinghe <104724063+PasiduRanasinghe@users.noreply.github.com>
2025-05-25 20:52:04 +0530

    Exercise 2.6 final

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
 
-  const handleOnchangeInput = (event) =>{
- setNewName(event.target.value)
-  }
-  const handleSubmit = ()=>{
-    setPersons(persons.concat(newName));
-  }
+  const handleOnchangeInput = (event) => {
+    setNewName(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setPersons([...persons, { name: newName}]);
+  };
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          name: <input onChange={handleOnchangeInput}/>
+          name: <input value={newName} onChange={handleOnchangeInput} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <ul>{persons.map((name)=>(
-        <li>{name}</li>
-      ))}</ul>
+      <ul>
+        {persons.map((person) => (
+          <li key={person.name}>{person.name}</li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
