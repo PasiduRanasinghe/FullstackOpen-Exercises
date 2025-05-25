b758153edd560015cb8cac36ce5845d20a633042
Pasidu Ranasinghe <104724063+PasiduRanasinghe@users.noreply.github.com>
2025-05-25 21:51:17 +0530

    Exercise 2.7

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,13 +3,26 @@ import { useState } from "react";
 const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
+  //check for matches and return true if there is a match
+  const checkPersonsMatches = (name) => {
+    const hasMatch = persons.some((person) => person.name === name);
+    return hasMatch;
+  };
 
+  //handle input element data
   const handleOnchangeInput = (event) => {
     setNewName(event.target.value);
   };
+
+  //handle the add button click and add value to the array
   const handleSubmit = (event) => {
     event.preventDefault();
-    setPersons([...persons, { name: newName}]);
+    if (checkPersonsMatches(newName)) {
+      window.alert(`${newName} is already added to phonebook`);
+    } else {
+      setPersons([...persons, { name: newName }]);
+    }
+    setNewName("");//clear the input element value
   };
   return (
     <div>
@@ -23,11 +36,10 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-
-        {persons.map((person) => (
-          <div key={person.name}>{person.name}</div>
-        ))}
-
+      {/* display persons in persons array */}
+      {persons.map((person) => (
+        <div key={person.name}>{person.name}</div>
+      ))}
     </div>
   );
 };
