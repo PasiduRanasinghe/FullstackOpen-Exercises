d0d8700e7dc4718823a8fb0d7779074377113176
Pasidu Ranasinghe <104724063+PasiduRanasinghe@users.noreply.github.com>
2025-05-26 23:50:46 +0530

    Exercise 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,9 +1,17 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas", number: 771122369 }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
   const [newName, setNewName] = useState("");
+  const [filteredPhones, setFilteredPhones] = useState(persons);
   const [newNumber, setNewNumber] = useState();
+  
+  
   //check for name matches and return true if there is a match
   const checkPersonsMatches = (name) => {
     const hasMatch = persons.some((person) => person.name === name);
@@ -11,10 +19,14 @@ const App = () => {
   };
 
   //handle input element data name
+  const handleFilter = (event) => {
+    setFilteredPhones(persons.filter(person => person.name.includes(event.target.value)))
+  };
+  //handle input element data name
   const handleOnchangeInputName = (event) => {
     setNewName(event.target.value);
   };
-  //handle input element data number 
+  //handle input element data number
   const handleOnchangeInputNumber = (event) => {
     setNewNumber(event.target.value);
   };
@@ -25,20 +37,34 @@ const App = () => {
     if (checkPersonsMatches(newName)) {
       window.alert(`${newName} is already added to phonebook`);
     } else {
-      setPersons([...persons, { name: newName, number: newNumber }]);
+      setPersons([
+        ...persons,
+        { name: newName, number: newNumber, id: persons.length + 1 },
+      ]);
     }
-    setNewName("");//clear the input element value
-    setNewNumber("");//clear the input element value
+    setNewName(""); //clear the input element value
+    setNewNumber(""); //clear the input element value
   };
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input onChange={handleFilter} type="text" />
+      </div>
+      <h2>Add a new</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          name: <input required value={newName} onChange={handleOnchangeInputName} />
+          name:{" "}
+          <input required value={newName} onChange={handleOnchangeInputName} />
         </div>
         <div>
-          number: <input required type='number' value={newNumber} onChange={handleOnchangeInputNumber} />
+          number:{" "}
+          <input
+            required
+            type="number"
+            value={newNumber}
+            onChange={handleOnchangeInputNumber}
+          />
         </div>
         <div>
           <button type="submit">add</button>
@@ -46,8 +72,10 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       {/* display persons in persons array */}
-      {persons.map((person) => (
-        <div key={person.name}>{person.name} {person.number}</div>
+      {filteredPhones.map((person) => (
+        <div key={person.id}>
+          {person.name} {person.number}
+        </div>
       ))}
     </div>
   );
