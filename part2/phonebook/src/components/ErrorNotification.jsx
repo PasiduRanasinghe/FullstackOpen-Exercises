2adcb073a36d652ddd58bd20f97f4bafa80fd524
Pasidu Ranasinghe <104724063+PasiduRanasinghe@users.noreply.github.com>
2025-07-29 00:28:28 +0530

    Exercise 2.17

---
+++ part2/phonebook/src/components/ErrorNotification.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+const successStyle = {
+  color: "red",
+  background: "lightgrey",
+  fontSize: "20px",
+  borderStyle: "solid",
+  borderRadius: "5px",
+  padding: "10px",
+  marginBottom: "10px",
+};
+function ErrorNotification({ message }) {
+  if (message === null) {
+    return null;
+  }
+  return <div style={successStyle}>{message}</div>;
+}
+
+export default ErrorNotification;
