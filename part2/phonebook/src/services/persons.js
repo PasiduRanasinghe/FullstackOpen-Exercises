18d295313aa772bf5812a5694a92d52f39f2c697
Pasidu Ranasinghe <104724063+PasiduRanasinghe@users.noreply.github.com>
2025-07-28 22:49:59 +0530

    Exercise 2.15

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -2,26 +2,26 @@ import axios from "axios";
 const baseUrl = "http://localhost:3001/persons";
 //get all persons
 const getAll = () => {
-   const request =  axios.get(baseUrl);
-   return request.then(response => response.data)
+  const request = axios.get(baseUrl);
+  return request.then((response) => response.data);
 };
 //create new person
-const create = newObject => {
-  const request =  axios.post(baseUrl, newObject);
-  return request.then(response => response.data)
+const create = (newObject) => {
+  const request = axios.post(baseUrl, newObject);
+  return request.then((response) => response.data);
 };
 
 //delete a person
 const deletePerson = (id) => {
-   const request =  axios.delete(`${baseUrl}/${id}`);
-   return request.then(response => response.data)
+  const request = axios.delete(`${baseUrl}/${id}`);
+  return request.then((response) => response.data);
 };
 
 //update a person
-const update = (id,newObject) =>{
- const request =  axios.put(`${baseUrl}/${id}`, newObject);
- return request.then(response => response.data)
-}
+const update = (id, newObject) => {
+  const request = axios.put(`${baseUrl}/${id}`, newObject);
+  return request.then((response) => response.data);
+};
 export default {
   getAll,
   create,
